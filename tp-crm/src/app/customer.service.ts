8a8324cdba6a7f7d9d63fec19f8803f67573def3
chems meziou <chemsmeziou@chemss-MacBook-Air.local>
2024-10-04 11:21:26 +0300

    Ajout de Bootstrap au projet et finalisation de la configuration de HttpClient

--- tp-crm/src/app/customer.service.ts
+++ tp-crm/src/app/customer.service.ts
@@ -11,6 +11,6 @@ export class CustomerService {
   private apiUrl = 'http://localhost:8080/api/customer';
 
   getCustomers(): Observable<Customer[]> {
-    return this.http.get<Customer[]>(`${this.apiUrl}/customers`);
+    return this.http.get<Customer[]>(`${this.apiUrl}`);
   }
 }
